ce034a6e2888e55b8d1a66646d6ff680534baeb3
The Wrench <123601564+TateTheBait@users.noreply.github.com>
2024-09-05 09:53:21 -0500

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,4 +1,1 @@
-local file = ('imports/%s.lua'):format(IsDuplicityVersion() and 'server' or 'client')
-local import = LoadResourceFile('ox_core', file)
-local chunk = assert(load(import, ('@@ox_core/%s'):format(file)))
-chunk()
+
