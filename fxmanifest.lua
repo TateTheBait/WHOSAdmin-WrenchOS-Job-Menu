e6969a1603e6f6f1fa72cdd03718421ab341acae
The Wrench <123601564+TateTheBait@users.noreply.github.com>
2024-09-05 09:54:15 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -17,15 +17,9 @@ files {
 }
 
 shared_script {
-    '@ox_lib/init.lua',
     "config.lua"
 }
 
-server_scripts {'@oxmysql/lib/MySQL.lua'}
 client_scripts {
     "client.lua",
-}
-
-dependencies {
-    "ox_lib"
 }
